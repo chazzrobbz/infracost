919787240463b62fb551bba461c95fa780d7964e
Hugo <hugorut@gmail.com>
2022-06-10 10:20:03 +0100

    fix: handle mocked outputs that are to be set on list resources and within functional expressions

--- internal/hcl/block.go
+++ internal/hcl/block.go
@@ -242,11 +242,13 @@ type Block struct {
 	childBlocks Blocks
 	// verbose determines whether the block uses verbose debug logging.
 	verbose  bool
+	newMock  func(attr *Attribute) cty.Value
 	Filename string
 }
 
 // BlockBuilder handles generating new Blocks as part of the parsing and evaluation process.
 type BlockBuilder struct {
+	MockFunc      func(a *Attribute) cty.Value
 	SetAttributes []SetAttributesFunc
 }
 
@@ -274,6 +276,7 @@ func (b BlockBuilder) NewBlock(filename string, hclBlock *hcl.Block, ctx *Contex
 			moduleBlock: moduleBlock,
 			childBlocks: children,
 			verbose:     isLoggingVerbose,
+			newMock:     b.MockFunc,
 		}
 	}
 
@@ -289,6 +292,7 @@ func (b BlockBuilder) NewBlock(filename string, hclBlock *hcl.Block, ctx *Contex
 			moduleBlock: moduleBlock,
 			childBlocks: children,
 			verbose:     isLoggingVerbose,
+			newMock:     b.MockFunc,
 		}
 	}
 
@@ -302,6 +306,7 @@ func (b BlockBuilder) NewBlock(filename string, hclBlock *hcl.Block, ctx *Contex
 		moduleBlock: moduleBlock,
 		childBlocks: children,
 		verbose:     isLoggingVerbose,
+		newMock:     b.MockFunc,
 	}
 }
 
@@ -644,7 +649,7 @@ func (b *Block) GetAttributes() []*Attribute {
 	}
 
 	for _, attr := range b.getHCLAttributes() {
-		results = append(results, &Attribute{HCLAttr: attr, Ctx: b.context, Verbose: b.verbose})
+		results = append(results, &Attribute{newMock: b.newMock, HCLAttr: attr, Ctx: b.context, Verbose: b.verbose})
 	}
 
 	return results
